011eb0bcf0b563b4cf1faeca8ab03b56b9e3296c
cecilRac <kobebrainght@yahoo.com>
2018-01-26 13:46:39 +0100

    stage 2 done. TODO: follow PSR-x rules

--- backend/src/Client/Webapp/app.php
+++ backend/src/Client/Webapp/app.php
@@ -7,6 +7,7 @@ if (file_exists(ROOT_PATH.'/vendor/autoload.php') === false) {
 }
 require_once ROOT_PATH.'/vendor/autoload.php';
 
+include 'routes/surveyController.php'
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
@@ -19,45 +20,16 @@ $app->get('/', function () use ($app) {
     return 'Status OK';
 });
 
-$app->get('/survey', function (Request $request) {
-    $data = $request->get('code');
 
-    return new Response($data, 200);
+$app->get('/api/v1/surveys', function () use($app) {
+    return $app->storage(0);
 });
 
-// Stage 1
-
-$app->post('/surveys', function () {
-    $reply = storage(0);
-    return new Response(json_encode($reply), 200);
+$app->get('/api/v1/survey', function (Request $request) use($app) {
+    $code = $request->get('code');   
+    return $app->groupSurveys($code);
 });
 
-//Stage 2
 
-function storage($value){
-    $surveys = [];
-    $uniqueSurveys = [];
-    $dir ='data';
-    $surveyFiles = array_diff(scandir($dir, 1), array('..', '.'));
-
-    foreach($surveyFiles as $independentSurvey){
-        $surveyJsonObject = json_decode( file_get_contents($dir.'/'.$independentSurvey));
-        array_push($surveys, $surveyJsonObject);
-        if(!in_array($surveyJsonObject->survey, $uniqueSurveys) && $surveyJsonObject !=null){
-            array_push($uniqueSurveys, $surveyJsonObject->survey);
-        }
-    }
-    if($value == 1){
-        return $surveys;
-    }return $uniqueSurveys;
-}
-
-function groupSurveys(){
-
-    $surveys = storage(1);
-    foreach($surveys as $independentSurvey){
-
-    }
-}
 $app->run();
 return $app;
